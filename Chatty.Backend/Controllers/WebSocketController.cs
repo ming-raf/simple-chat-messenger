5d2ddb4fefa5620f86ca8f3c0d5d878a3e038f10
rafiqirosli <fricheps@Hotmail.com>
2022-10-14 10:58:23 +1030

    initial commit

---
+++ Chatty.Backend/Controllers/WebSocketController.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatty.Backend.Controllers;
+
+// <snippet>
+public class WebSocketController : ControllerBase
+{
+    private readonly ILogger _logger;
+    private ICollection<string> _chatMessages = new List<string>();
+
+    public WebSocketController(ILogger<WebSocketController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/ws")]
+    public async Task Get()
+    {
+        var sourceIp = Request.HttpContext.Connection.RemoteIpAddress;
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            _logger.LogInformation("WebSocket connected from " + sourceIp);
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            await Echo(webSocket);
+        }
+        else
+        {
+            _logger.LogDebug("Not a WebSocket request from " + sourceIp);
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    // </snippet>
+
+    private async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var receiveResult = await webSocket.ReceiveAsync(
+            new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!receiveResult.CloseStatus.HasValue)
+        {
+            //await webSocket.SendAsync(
+            //    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+            //    receiveResult.MessageType,
+            //    receiveResult.EndOfMessage,
+            //    CancellationToken.None);
+
+            await StoreMessage(buffer, receiveResult);
+
+            var staticResponse = new ArraySegment<byte>(Encoding.UTF8.GetBytes("stfu dood"));
+            await webSocket.SendAsync(
+                staticResponse,
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None);
+
+            receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(
+            receiveResult.CloseStatus.Value,
+            receiveResult.CloseStatusDescription,
+            CancellationToken.None);
+    }
+
+    private async Task StoreMessage(byte[] buffer, WebSocketReceiveResult webSocketReceiveResult)
+    {
+        var message = Encoding.ASCII.GetString(buffer, 0, webSocketReceiveResult.Count);
+        
+        _logger.LogInformation(message);
+        _chatMessages.Add(message);
+    }
+}
---
+++ Chatty/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
